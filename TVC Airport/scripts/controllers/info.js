d435573508c9f2201785c0f1830d8f150be17e63
sundbe10 <sundbe10@gmail.com>
2014-10-11 19:24:00 -0700

    More files

--- TVC Airport/scripts/controllers/info.js
+++ TVC Airport/scripts/controllers/info.js
@@ -12,5 +12,17 @@ angular.module('tvcairport').controller('InfoCtrl', ['$scope','$rootScope','$rou
     	console.log($routeParams);
     	$scope.infoPage = "views/info/" + $routeParams.page + ".html";
     	
+    	
+    	$timeout(function(){
+    		console.log("test")
+    		var phoneNumbers = $(".phone");
+    		$.each(phoneNumbers, function(index,value){
+				console.log(value);
+				var origNumber = $(value).text();
+				var strippedNumber = origNumber.replace(/\D/g,'');
+				$(value).html("<a href='tel:"+strippedNumber+"'>"+origNumber+"</a>");
+			})
+    	},500)
+		
 	}
 ])
