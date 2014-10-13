cb9d02d312cc75a0a0c64d6065247ee807f852ca
sundbe10 <sundbe10@gmail.com>
2014-10-12 18:27:08 -0700

    My trips functionality working

--- TVC Airport/scripts/app.js
+++ TVC Airport/scripts/app.js
@@ -4,16 +4,25 @@ angular.module('configuration', []);
 angular.module('tvcairport', ['ngRoute','ngAnimate','ngSanitize','configuration'])
 .config(function ($routeProvider, $sceDelegateProvider) {
 	$routeProvider
-	.when('/flights', {
-		templateUrl: 'views/flights-menu.html'
-	}).when('/info', {
+	.when('/info', {
 		templateUrl: 'views/info-menu.html'
-	}).when('/frame/:frame', {
-		templateUrl: 'views/frame.html',
-		controller: 'FrameCtrl'
 	}).when('/info/:page', {
 		templateUrl: 'views/info.html',
 		controller: 'InfoCtrl'
+	}).when('/flights', {
+		templateUrl: 'views/flights-menu.html'
+	}).when('/frame/:frame', {
+		templateUrl: 'views/frame.html',
+		controller: 'FrameCtrl'
+	}).when('/my-trips', {
+		templateUrl: 'views/my-trips-menu.html',
+		controller: 'TripsCtrl'
+	}).when('/my-trips/:page', {
+		templateUrl: 'views/my-trips.html',
+		controller: 'TripsCtrl'
+	}).when('/my-trips/:page/:type', {
+		templateUrl: 'views/my-trips.html',
+		controller: 'TripsCtrl'
 	}).otherwise({
 		templateUrl: 'views/home.html'
 	})
